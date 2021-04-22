798f1dd7170ac010375a529343500de640d6b747
nick <nick@nickdown.com>
2021-04-21 23:02:22 -0400

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -7,61 +7,68 @@ import { Floor } from './tiles'
 
 const tiles = Level1.getLayout()
 
+function updateChipImage(event: KeyboardEvent, setChipImage: any) {
+  switch (event.code) {
+    case 'ArrowLeft':
+      setChipImage(playerLeft)
+      break;
+    case 'ArrowRight':
+      setChipImage(playerRight)
+      break;
+    case 'ArrowDown':
+      setChipImage(playerDown)
+      break;
+    case 'ArrowUp':
+      setChipImage(playerUp)
+      break;
+    default:
+      break;
+  }
+}
+
+function calculateNextPosition(chip: { col: number; row: number }, event: KeyboardEvent) {
+  const nextPosition = { ...chip }
+  switch (event.code) {
+    case 'ArrowLeft':
+      nextPosition.col = nextPosition.col - 1
+      break;
+    case 'ArrowRight':
+      nextPosition.col = nextPosition.col + 1
+      break;
+    case 'ArrowDown':
+      nextPosition.row = nextPosition.row + 1
+      break;
+    case 'ArrowUp':
+      nextPosition.row = nextPosition.row - 1
+      break;
+    default:
+      break
+  }
+  return nextPosition
+}
+
+const generateCell = (rowIndex: number, colIndex: number, chip: { row: number; col: number; }, chipImage: string) => {
+  if (chip.row === rowIndex && chip.col === colIndex) {
+    return <img src={ chipImage } alt=''/>
+  }
+
+  return tiles[rowIndex][colIndex].html()
+}
+
 function App() {
-  const [chip, setChip] = useState({row: 7, col: 8})
+  const [playerPosition, setPlayerPosition] = useState({row: 7, col: 8})
   const [chipImage, setChipImage] = useState(playerDown)
   const [numberOfChips, setNumberOfChips] = useState(0)
   const [numberOfChipsRequired, setNumberOfChipsRequired] = useState(11)
   const [keys, setKeys] = useState({ red: 0, green: 0, blue: 0, yellow: 0 })
 
-  const getters = { chip, chipImage, numberOfChips, keys, numberOfChipsRequired }
-  const setters = { setChip, setChipImage, setNumberOfChips, setKeys, setNumberOfChipsRequired }
-
-
-  const generateCell = (rowIndex: number, colIndex: number, chip: { row: number; col: number; }) => {
-    if (chip.row === rowIndex && chip.col === colIndex) {
-      return <img src={ chipImage } alt=''/>
-    }
-
-    return tiles[rowIndex][colIndex].html()
-  }
+  const getters = { playerPosition, chipImage, numberOfChips, keys, numberOfChipsRequired }
+  const setters = { setPlayerPosition, setChipImage, setNumberOfChips, setKeys, setNumberOfChipsRequired }
 
   useEffect(() => {
     function updateGame(event: KeyboardEvent) {
-      const nextPosition = {...chip}
-      switch (event.code) {
-        case 'ArrowLeft':
-          setChipImage(playerLeft)
-          break;
-        case 'ArrowRight':
-          setChipImage(playerRight)
-          break;
-        case 'ArrowDown':
-          setChipImage(playerDown)
-          break;
-        case 'ArrowUp':
-          setChipImage(playerUp)
-          break;
-        default:
-          return
-      }
-
-      switch (event.code) {
-        case 'ArrowLeft':
-          nextPosition.col = nextPosition.col - 1
-          break;
-        case 'ArrowRight':
-          nextPosition.col = nextPosition.col + 1
-          break;
-        case 'ArrowDown':
-          nextPosition.row = nextPosition.row + 1
-          break;
-        case 'ArrowUp':
-          nextPosition.row = nextPosition.row - 1
-          break;
-        default:
-          return
-      }
+      updateChipImage(event, setChipImage)
+      const nextPosition = calculateNextPosition(playerPosition, event)
 
       try {
         const tile = tiles[nextPosition.row][nextPosition.col]
@@ -71,7 +78,7 @@ function App() {
       }
 
       tiles[nextPosition.row][nextPosition.col] = new Floor()
-      setChip(nextPosition)
+      setPlayerPosition(nextPosition)
     }
 
     document.addEventListener('keyup', updateGame)
@@ -85,7 +92,7 @@ function App() {
       <div id='board'>
         {tiles.map((row, rowIndex) => {
           return row.map((cell, colIndex) => {
-            return generateCell(rowIndex, colIndex, chip)
+            return generateCell(rowIndex, colIndex, playerPosition, chipImage)
           })
         })}
       </div>
