9b1dfd084a6dd5bc85fa2a239e84f99c93966e07
nick <nick@nickdown.com>
2021-04-23 00:48:15 -0400

    Add swimming

--- src/tiles/water/Water.tsx
+++ src/tiles/water/Water.tsx
@@ -1,8 +1,26 @@
 import water from "../../assets/tiles/water/water.png";
+import player_swim_down from "../../assets/tiles/water/player_swim_down.png";
+import player_swim_up from "../../assets/tiles/water/player_swim_up.png";
+import player_swim_left from "../../assets/tiles/water/player_swim_left.png";
+import player_swim_right from "../../assets/tiles/water/player_swim_right.png";
 
 export default class Water {
-  html() {
-    return <img src={water} alt=''/>
+  html(rowIndex, colIndex, getters) {
+    if (getters.playerPosition.row === rowIndex && getters.playerPosition.col === colIndex) {
+      switch (getters.chipDirection) {
+        case "DOWN":
+          return <img src={ player_swim_down } alt=''/>
+        case "UP":
+          return <img src={ player_swim_up } alt=''/>
+        case "LEFT":
+          return <img src={ player_swim_left } alt=''/>
+        case "RIGHT":
+          return <img src={ player_swim_right } alt=''/>
+        default:
+          return <img src={ water } alt=''/>
+      }
+    }
+    return <img src={ water } alt=''/>
   }
 
   enter(getters, setters) {
